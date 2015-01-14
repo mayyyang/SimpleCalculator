43b6d58e2b152dc3c6eefca3f3f89bb1fb851082
May <mayy.yang@gmail.com>
2015-01-14 00:13:24 -0800

    Initial Commit

---
+++ MultiplyChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiplyChallenge
+//
+//  Created by May Yang on 1/14/15.
+//  Copyright (c) 2015 May Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiplyChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultiplyChallenge
+//
+//  Created by May Yang on 1/14/15.
+//  Copyright (c) 2015 May Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
